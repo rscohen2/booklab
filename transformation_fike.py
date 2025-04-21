801aec22f75ced3007a76171b84e3a6c154a966e
Becca Cohen <145156789+rscohen2@users.noreply.github.com>
2025-04-21 11:06:07 -0500

    adding intial code

--- transformation_fike.py
+++ transformation_fike.py
@@ -2,6 +2,14 @@
 import string
 import random
 
+#have to repeat translation tho...
+# Initialize EasyNMT for translation
+from easynmt import EasyNMT
+
+model_name = 'mbart50_m2m'
+# Create the translator
+translator = EasyNMT(model_name=model_name, language_detection = 'fasttext')
+
 string22 = ("I was talking of ladies smiling in the eyes of gentlemen; and of late so many smiles have been shed into Mr. Rochester’s eyes that they overflow like two cups filled above the brim: have you never remarked that?")
 
 string2 =" The flame flickers in the eye; the eye shines like dew; it looks soft\
@@ -16,16 +24,34 @@ confirm me in my opinion. The eye is favourable."
 
 
 def replace_word(string, term):
-    length = len(string.split(' '))
     word = random.choice(string.split(' '))
     string = string.replace(word, term)
     return string, word
 
-def translate_word(word):
+def translate_word_to_en(word):
+    translated_word= translator.translate(word, target_lang='en')
+    return translated_word
 
-    pass
+
+def translate_word_to_it(word):
+    translated_word = translator.translate(word, target_lang='it')
+    return translated_word
 
 print(replace_word(string22, 'chicken'))
+
+en_term = random.choice(string22.split(' '))
+
+term_in_it = translate_word_to_it(en_term)
+
+term_in_en = translate_word_to_en(term_in_it)
+
+
+print(replace_word(string22, term_in_en))
+
+
+
+
+
 
 
 
